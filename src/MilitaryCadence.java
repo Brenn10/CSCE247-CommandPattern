31ff2e82c2c91360635918f4ebb86a64eb2e19cd
Robert Sturman <tssturman@gmail.com>
2021-10-12 15:20:15 -0400

    Created helper fileReader

--- src/MilitaryCadence.java
+++ src/MilitaryCadence.java
@@ -1,5 +1,9 @@
 package src;
 
+import java.io.File;
+import java.util.Scanner;
+
+// Trey and Ian's part
 public class MilitaryCadence {
 
     public MilitaryCadence() {
@@ -16,5 +20,19 @@ public class MilitaryCadence {
 
     public void singInArmy() {
         
+    }
+    /** 
+     * @author Trey Sturman
+     * reads a file from the specified location and returns it as a string
+     * @return file as a string
+     */ 
+    private String fileReader(String fileName) {
+        String fileasString = "";
+        File file = new File(fileName);
+        Scanner fileScanner = new Scanner(file);
+        while (fileScanner.hasNextLine()) {
+            fileasString += fileScanner.nextLine();
+        }
+        return fileasString;
     }
 }
