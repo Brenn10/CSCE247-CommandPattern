bbd1d58bac056236676b203de57778040b8c8b20
Stella Garcia <stellae13@gmail.com>
2021-10-12 15:42:38 -0400

    Fixed pulling mistakes

--- src/MilitaryCadence.java
+++ src/MilitaryCadence.java
@@ -8,10 +8,6 @@ import java.util.Scanner;
 
 // Trey and Ian's part
 public class MilitaryCadence {
-<<<<<<< HEAD
-
-=======
->>>>>>> 24efd8f400590d6f71e764364e3ce21a7cefa215
     public MilitaryCadence() {
 
     }
@@ -32,16 +28,6 @@ public class MilitaryCadence {
      * reads a file from the specified location and returns it as a string
      * @return file as a string
      */ 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    /*private void fileReader(String fileName) {
-=======
-    private ArrayList<String> fileReader(String fileName) {
-        ArrayList <String> fileasString;
->>>>>>> 1ad17599bb211e70d3929c537d612acb019e7815
-        File file = new File(fileName);
-        Scanner fileScanner = new Scanner(file);
-=======
     private List<String> fileReader(String fileName) {
         Scanner fileScanner;
         try {
@@ -50,18 +36,9 @@ public class MilitaryCadence {
             return null;
         }
         ArrayList<String> fileLines = new ArrayList<String>();
->>>>>>> 24efd8f400590d6f71e764364e3ce21a7cefa215
         while (fileScanner.hasNextLine()) {
             fileLines.add(fileScanner.nextLine());
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-    }*/
-=======
-        return fileasString;
-=======
         return fileLines;
->>>>>>> 24efd8f400590d6f71e764364e3ce21a7cefa215
     }
->>>>>>> 1ad17599bb211e70d3929c537d612acb019e7815
 }
