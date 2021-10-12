ee3f972a0e359d4fba0fb2dbc4506bff9a8d8365
Robert Sturman <tssturman@gmail.com>
2021-10-12 15:27:43 -0400

    altered filereader to go into an attribute of the Military Cadence

--- src/MilitaryCadence.java
+++ src/MilitaryCadence.java
@@ -1,11 +1,12 @@
 package src;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 // Trey and Ian's part
 public class MilitaryCadence {
-
+private ArrayList <String> fileasString;
     public MilitaryCadence() {
 
     }
@@ -26,13 +27,11 @@ public class MilitaryCadence {
      * reads a file from the specified location and returns it as a string
      * @return file as a string
      */ 
-    private String fileReader(String fileName) {
-        String fileasString = "";
+    private void fileReader(String fileName) {
         File file = new File(fileName);
         Scanner fileScanner = new Scanner(file);
         while (fileScanner.hasNextLine()) {
-            fileasString += fileScanner.nextLine();
+            fileasString.add(fileScanner.nextLine());
         }
-        return fileasString;
     }
 }
